de583c0a768f9b40da2cd1c969cb6533dd8cfae2
zooyaam <yjoo2498@naver.com>
2023-11-21 17:33:23 +0900

    초기 구성

---
+++ web-cafe/tailwind.config.js
@@ -0,0 +1,10 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  port: 8088,
+  root: "src/",
+  open: ["index.html"],
+  host: "localhost",
+  browser: "chrome",
+  https: false,
+  watch: "src/",
+};
