b8556c45d6d6ed408c9b41f7ab7c36e2e5db1a19
zooyaam <yjoo2498@naver.com>
2023-11-22 19:22:07 +0900

    기본 설정

---
+++ web-cafe/fiveserver.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  port: 8088,
+  root: "src/",
+  open: ["header.html"],
+  host: "localhost",
+  browser: "chrome",
+  https: false,
+  watch: "src/",
+};
--- web-cafe/tailwind.config.js
+++ web-cafe/tailwind.config.js
@@ -1,10 +1,1 @@
-/* @type {import('tailwindcss').Config} */
-module.exports = {
-  port: 8088,
-  root: "src/",
-  open: ["index.html"],
-  host: "localhost",
-  browser: "chrome",
-  https: false,
-  watch: "src/",
-};
+
