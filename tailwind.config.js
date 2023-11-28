b13e8874eab312fcf472ef5a6aadfbec5373c29d
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2023-11-28 18:14:16 +0900

    애니메이션 작업

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./cafe/src/**/*.html'],
+  content: ["./cafe/src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "flower-01": "url('/images/ani_flower_01.png')",
+        "flower-02": "url('/images/ani_flower_02.png')",
+        "flower-03": "url('/images/ani_flower_03.png')",
+        "flower-04": "url('/images/ani_flower_04.png')",
+        "flower-bg": "url('/images/bg_flower.png')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
